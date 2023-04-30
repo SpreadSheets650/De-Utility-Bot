c07cdb7bc9674d6dbb531ad75ecc195fbc643e6d
Soham Maity <89208925+SpreadSheets650@users.noreply.github.com>
2023-04-30 23:24:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,57 @@
-import os
+# ------ Modules ------ #
 
 import discord
+import datetime
+import settings
+import keep_alive
 from discord.ext import commands
 
-DISCORD_API_TOKEN = os.getenv('TOKEN')
+
+# ------ Main Method ------ #
+
+logger = settings.logging.getLogger("bot")
 
 
 def run():
     intents = discord.Intents.default()
-
+    
     bot = commands.Bot(command_prefix="!", intents=intents)
 
     @bot.event
     async def on_ready():
-        print(f'BOT ID : {bot.user.id}')
-        print(f'BOT Name : {bot.user}')
-        print("-------------- BOT ONLINE --------------")
 
-    bot.run(DISCORD_API_TOKEN)
+        bot.start_time = datetime.datetime.now()
 
+        print("----------------- BOT INFO -----------------")
+        logger.info(f"ID: {bot.user.id}")
+        logger.info(f"User: {bot.user}")
+        print("---------------- BOT ONLINE ----------------")
+
+  
+
+    @bot.command(
+      aliases = ['p']
+    )
+    async def ping(ctx):
+      
+        latency = bot.latency * 1000
+        server_name = ctx.guild.name if ctx.guild else "Direct Message"
+        uptime = datetime.datetime.now() - bot.start_time
+        uptime_seconds = uptime.total_seconds()
+        uptime_str = str(datetime.timedelta(seconds=uptime_seconds)).split(".")[0]
+        num_servers = len(bot.guilds)
+        timestamp = ctx.message.created_at.strftime("%m/%d/%Y %I:%M %p")
+
+        await ctx.send(f"_*Pong !*_\n\n> Servers: {num_servers}\n> Latency: {latency:.2f}ms\n> Server Name: {server_name}\n\n`Uptime: {uptime_str}`\n`{timestamp}`")
+
+    @bot.command()
+    async def love(ctx):
+
+        await ctx.send(f"I Still Love You <@881073499429552168> :heart:")
+
+    bot.run(settings.DISCORD_API_TOKEN)
+    
+keep_alive.keep_alive()
 
 if __name__ == "__main__":
     run()
